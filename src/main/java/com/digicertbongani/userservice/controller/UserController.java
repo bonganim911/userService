3752b3c73967364cdb2c51d887c128714cc30e39
user <user@users-MacBook-Air.local>
2024-02-06 22:34:35 +0200

    Added the get user by id on the controller and tests.

--- src/main/java/com/digicertbongani/userservice/controller/UserController.java
+++ src/main/java/com/digicertbongani/userservice/controller/UserController.java
@@ -3,7 +3,9 @@ package com.digicertbongani.userservice.controller;
 import com.digicertbongani.userservice.model.User;
 import com.digicertbongani.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,14 @@ public class UserController {
     @GetMapping
     public List<User> getUsers(){
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId){
+        User user = userService.getUser(userId);
+        if(user != null){
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/test/java/com/digicertbongani/userservice/controller/userControllerTest.java
+++ src/test/java/com/digicertbongani/userservice/controller/userControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -38,5 +39,29 @@ public class userControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/users"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)));
+    }
+
+    @Test
+    public void shouldReturnUserGivenId() throws Exception {
+        User mockUser = new User(1L, "Eric","Thomas","erict@user.com");
+        when(userService.getUser(mockUser.getId())).thenReturn(mockUser);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", Matchers.is("Eric")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is("erict@user.com")));
+
+    }
+
+    @Test
+    public void shouldReturnUserNotFoundGivenUserIdDoesExist() throws Exception {
+
+        when(userService.getUser(0L)).thenReturn(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/1"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
     }
 }
